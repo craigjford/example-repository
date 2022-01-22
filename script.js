51d5c7aec9590b0371739acb41aa1902217093ec
Craig Ford <craig_ford@icloud.com>
2022-01-22 12:57:14 -0500

    added click event handler

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ const h2 = document.createElement("h2");
 document.querySelector("body").appendChild(h2);
 document.querySelector("h2").textContent = "This content added by JavaScript";
 
-document.getElementById("picshow").addEventListener("click"), function () {
-    this.style.display = 'none';
-    document.getElementById("pichide").style.display='block';
-    document.getElementById("photo").style.display='block';
+function mousestatus(e) {
+    if (!e) e = window.event;
+    btn = e.button;
+    switch(btn) {
+        case 0:
+            whichone = "Left";
+            break;
+        case 1:
+            whichone = "Middle";
+            break;
+        case 2:
+            whichone = "Right";
+            break;
+        default:
+            whichone = "UNKNOWN";
+    }
+    message = e.type + " : " + whichone + "<br>";
+    document.getElementById('testarea').innerHTML += message;
+    e.preventDefault();
 }
+obj = document.getElementById('testlink');
 
-document.getElementById("pichide").addEventListener("click"), function () {
-    this.style.display = 'none';
-    document.getElementById("picshow").style.display='block';
-    document.getElementById("photo").style.display='none';
-}
+obj.addEventListener("click", mousestatus);
+obj.addEventListener("mousedown", mousestatus);
+obj.addEventListener("mouseup", mousestatus);
+obj.addEventListener("dblclick", mousestatus);
+
 
 
 
