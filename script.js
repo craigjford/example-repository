2554320d70c048d532c07414f49f6eb67b63eebd
Craig Ford <craig_ford@icloud.com>
2022-01-21 20:48:27 -0500

    click event code

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const h2 = document.createElement("h2");
 document.querySelector("body").appendChild(h2);
 document.querySelector("h2").textContent = "This content added by JavaScript";
 
+document.getElementById("picshow").addEventListener("click"), function () {
+    this.style.display = 'none';
+    document.getElementById("pichide").style.display='block';
+    document.getElementById("photo").style.display='block';
+}
+
+document.getElementById("pichide").addEventListener("click"), function () {
+    this.style.display = 'none';
+    document.getElementById("picshow").style.display='block';
+    document.getElementById("photo").style.display='none';
+}
+
+
