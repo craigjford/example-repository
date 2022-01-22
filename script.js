0ecd4b15a668e359ea34e8f2a3e8727574a8f35e
Craig Ford <craig_ford@icloud.com>
2022-01-22 13:51:29 -0500

    change click event handler with buttons

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const h2 = document.createElement("h2");
 document.querySelector("body").appendChild(h2);
 document.querySelector("h2").textContent = "This content added by JavaScript";
 
-
+var x=100, y=150;
+function pos(dx,dy) {
+    if (!document.getElementById) return;
+    x += 30*dx;
+    y += 30*dy;
+    obj = document.getElementById("square");
+    obj.style.top = y + "px";
+    obj.style.left = x + "px";
+}
+function hidesquare() {
+    if (!document.getElementById) return;
+    obj = document.getElementById("square");
+    obj.style.display="none";
+}
+function showsquare() {
+    if (!document.getElementById) return;
+    obj = document.getElementById("square");
+    obj.style.display="block";
+}
 
 
 
