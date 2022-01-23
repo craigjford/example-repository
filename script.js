3d7b056c47656772ee27cac1822653f0678c1678
Craig Ford <craig_ford@icloud.com>
2022-01-23 11:39:47 -0500

    changed x and y starting values again

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ document.querySelector("h2").textContent = "This content added by JavaScript";
 var x=0, y=0;
 function pos(dx,dy) {
     if (!document.getElementById) return;
-    console.log("before x = ${x} and ${y} and dx = ${dx} and dy = ${dy}")
     x += 30*dx;
     y += 30*dy;
-    console.log("after x = ${x} and ${y} and dx = ${dx} and dy = ${dy}")
     obj = document.getElementById("square");
     obj.style.top = y + "px";
     obj.style.left = x + "px";
